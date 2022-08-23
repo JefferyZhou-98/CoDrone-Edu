e12e93db6a1d2565c19ca8f3baa40ac0630d85d8
JefferyZhou-98 <jefferyzhou98@gmail.com>
2022-08-22 22:50:10 -0400

    simple take off completed

--- simple_takeoff.py
+++ simple_takeoff.py
@@ -1,14 +1,23 @@
 from codrone_edu.drone import *
+import numpy as np
+
 drone = Drone()
+print("Getting Ready to Pair...")
 drone.pair()
+print("Paired!")
+#? Setting Trim --------------
 # if the drone is drifting right, so trim to roll left a little bit
 # this alters the thrust/power input to the motors to counter drift
-drone.set_trim(-5, 0)
+drone.set_trim(0, 0)
 # viewing the current trim
-drone.get_trim()
+print(drone.get_trim())
+#? Setting Trim --------------
 # Add a time.sleep(1) before takeoff if you're planning to set the trim before takeoff
 time.sleep(1)
 drone.takeoff()
-drone.hover(3)
+print("Taking off...")
+drone.hover(5)
+print("Hovering...")
 drone.land()
+print("Landing...")
 drone.close()
