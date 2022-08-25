17707e73c3fd29f05cfa8b41f6129d4d8de54e29
JefferyZhou-98 <jefferyzh98@gmail.com>
2022-08-25 14:05:47 -0400

    added usable GUI

---
+++ simple_tests/testing.py
@@ -0,0 +1,31 @@
+from codrone_edu.drone import *
+import numpy as np
+import sys
+sys.path.insert(0, r'C:\Users\zhou1340\Desktop\CoDrone_Edu\src')
+from user_GUI import user_input
+
+drone = Drone()
+print("Getting Ready to Pair...")
+drone.pair()
+print("Paired!")
+
+#? taking in user inputs
+speed, throttle, duration = user_input()
+
+#? Setting Trim --------------
+# if the drone is drifting right, so trim to roll left a little bit
+# this alters the thrust/power input to the motors to counter drift
+drone.set_trim(0, 0)
+# viewing the current trim
+print(drone.get_trim())
+#? Setting Trim --------------
+# Add a time.sleep(1) before takeoff if you're planning to set the trim before takeoff
+time.sleep(1)
+drone.takeoff()
+print("Taking off...")
+drone.hover(duration)
+print("Hovering...")
+drone.land()
+print("Landing...")
+drone.close()
+
